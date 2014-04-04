ee623f23011bf55e99171769bd123c6a304c797c
Katarzyna Turbiasz <kturbiasz@gmail.com>
2014-04-04 14:16:08 +0200

    improved version of calendar for month

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,15 +4,14 @@ class CalendarController < ApplicationController
   before_action :signed_in_user
   #before_action :set_meal_types, :set_dishes, :set_products
 
-  def day
-    @date = (params[:date] ? Date.strptime(params[:date], "%Y-%m-%d") : Date.strptime(Time.now.strftime("%Y-%m-%d")))
-    @menu = Menu.includes(meals: [{dish: [ingredients: :product]}, :meal_type]).find_by(menu_date: @date, user: @current_user)
-    @meals = @menu.meals.includes(:dishes) if @menu
-  end
+  #def day
+  #  @date = (params[:date] ? Date.strptime(params[:date], format: 'date.formats.custom') : Date.strptime(Time.now ))
+  #  @menu = Menu.includes(meals: [{dish: [ingredients: :product]}, :meal_type]).find_by(menu_date: @date, user: @current_user)
+  #  @meals = @menu.meals.includes(:dishes) if @menu
+  #end
 
   def month
-    @date = (params[:year_month] ? Date.strptime(params[:year_month] + '-01', "%Y-%m-%d") : Date.strptime(Time.now.strftime("%Y-%m-%d")))
+    @date = (params[:year_month] ? params[:year_month] : Time.now)
     @cal = CalendarMonth.new(@date)
   end
-
 end
--- lib/calendar_month.rb
+++ lib/calendar_month.rb
@@ -7,110 +7,20 @@ class CalendarMonth
     @date = date
   end
 
-  def declare_month_names
-    {1 => 'styczeń', 2 => 'luty', 3 => 'marzec', 4 => 'kwiecień', 5 => 'maj', 6 => 'czerwiec',
-     7 =>'lipiec', 8 => 'sierpień', 9 => 'wrzesień', 10 => 'październik', 11 => 'listopad', 12 => 'grudzień'}
-  end
+  def all_days_for_month_view
+    first_day = @date.beginning_of_month
+    last_day = @date.end_of_month
+    month_length = Time.days_in_month(@date.month, @date.year)
+    view_month = []
+    (weekday_number(first_day) - 1).downto(1) { |i| view_month << first_day.ago(i.days) } if weekday_number(first_day) > 1
+    0.upto(month_length - 1) { |i|  view_month << first_day.advance(days: i) }
+    1.upto(7 - weekday_number(last_day)) { |i| view_month << last_day.advance(days: i) } if weekday_number(last_day) < 7
 
-  def declare_weekday_names
-    {1 => 'poniedziałek', 2 => 'wtorek', 3 => 'środa', 4 => 'czwartek', 5 => 'piątek', 6 => 'sobota', 7 => 'niedziela'}
+    view_month.flatten.each_slice(7).to_a
   end
 
   def weekday_number(date)
-    date.wday == 0 ? 7 : date.wday
+    @weekday_number ||= {}
+    @weekday_number[date] ||= (date.wday == 0 ? 7 : date.wday)
   end
-
-  def count_days(date)
-    Time.days_in_month(date.month, date.year)
-  end
-
-  def present_month
-    months = declare_month_names
-    months[@date.month]
-  end
-
-  def build_weeks_array
-    weeks_number = count_number_of_weeks
-    first_week_date = first_week
-    first_day_of_second_week = ((first_week_date)[-1].split(" "))[0].to_i + 1
-    weeks_for_month = {}
-    weeks_for_month[0] = first_week_date
-    starting_step = 0
-    #while i < weeks_number - 1
-    1.upto(weeks_number-1) do |i|
-      weeks_for_month[i] = each_middle_week(first_day_of_second_week + starting_step)
-      starting_step += 7
-    end
-    weeks_for_month[weeks_number - 1] = final_week
-    weeks_for_month
-  end
-
-  def list_dates_to_show
-    months = declare_month_names
-    year = @date.year
-    beginning_day = first_week[0].split(" ")[0].to_i
-    ending_day = final_week[-1].split(" ")[0].to_i
-    beginning_month = months.invert[first_week[0].split(" ")[-1]].to_i
-    ending_month = months.invert[final_week[-1].split(" ")[-1]].to_i
-    beginning = Date.new(year, beginning_month, beginning_day)
-    ending = Date.new(year, ending_month, ending_day)
-    [beginning, ending]
-  end
-
-  def count_number_of_weeks
-    first_of_month = weekday_number(@date.beginning_of_month)
-    days_after_first_sunday = count_days(@date) - (8 - first_of_month)
-    (days_after_first_sunday % 7 > 0) ? (2 + days_after_first_sunday / 7) : (1 + days_after_first_sunday / 7)
-  end
-
-  def first_week
-    (list_end_days_for_month(@date)).concat(list_days_till_sunday(@date))
-  end
-
-  def each_middle_week(start_day)
-    months = declare_month_names
-    week = []
-    (0..6).each do |i|
-      week << "#{start_day + i} #{months[@date.month]}"
-    end
-    week
-  end
-
-  def final_week
-    if weekday_number(@date.end_of_month) == 7
-      start_day = 6.days.ago(@date.end_of_month).day
-      each_middle_week(start_day)
-    else
-      date_new = @date.end_of_month.tomorrow
-      (list_end_days_for_month(date_new)).concat(list_days_till_sunday(date_new))
-    end
-  end
-
-  def list_end_days_for_month(date)
-    months = declare_month_names
-    first_of_month = weekday_number(date.beginning_of_month)
-    prev_month_date = date.beginning_of_month.yesterday
-    days_in_prev_month = count_days(prev_month_date)
-    prev_month_days_list = []
-    while first_of_month > 1
-      prev_month_days_list << "#{days_in_prev_month} #{months[prev_month_date.month]}"
-      first_of_month -= 1
-      days_in_prev_month -=1
-    end
-    prev_month_days_list.reverse!
-  end
-
-  def list_days_till_sunday(date)
-    months = declare_month_names
-    first_of_month = weekday_number(date.beginning_of_month)
-    current_month_days_first_week = []
-    current_month_day = 1
-    until first_of_month > 7
-      current_month_days_first_week << "#{current_month_day} #{months[date.month]}"
-      current_month_day += 1
-      first_of_month += 1
-    end
-    current_month_days_first_week
-  end
-
 end
