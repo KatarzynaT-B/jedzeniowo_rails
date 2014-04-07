73642e0f315a2e82e1758f17a238bc090e6f2e76
Katarzyna Turbiasz <kturbiasz@gmail.com>
2014-04-07 11:10:46 +0200

    comments for calendar class

--- lib/calendar_month.rb
+++ lib/calendar_month.rb
@@ -7,6 +7,10 @@ class CalendarMonth
     @date = date
   end
 
+  #Depending on day of week for month beginning and end of the @date.month, method checks how many days of previous and next month should be displayed
+  #so that the calendar's view was filled. If @date.month begins on day other than Monday, the first date displayed should be weekday_number of this
+  #day minus 1, else - previous month is not included in view. If @date.month ends on day other than Sunday, the last date displayed should be
+  #7 minus weekday_number for this day, else - next month is not included in view.
   def all_days_for_month_view
     first_of_month = @date.beginning_of_month
     last_of_month = @date.end_of_month
@@ -20,6 +24,7 @@ class CalendarMonth
 
   protected
 
+    #Week starts on Monday (1), ends on Sunday (7)
     def weekday_number(date)
     @weekday_number ||= {}
     @weekday_number[date] ||= (date.wday == 0 ? 7 : date.wday)
